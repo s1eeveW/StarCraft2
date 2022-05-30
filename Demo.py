94917f2e7b901a5ba4329310115d20acff72e508
Hanwen_W <64772045+s1eeveW@users.noreply.github.com>
2022-05-30 11:41:05 +0800

    Add files via upload

--- Demo.py
+++ Demo.py
@@ -3,6 +3,9 @@ import os
 import csv
 import math
 
+# playerName = ""
+# playerName = input("Please enter the player's name:")
+
 # 一些自定义functions
 def remove_id(temp):
     name_id = str(temp)
@@ -81,25 +84,34 @@ def seconds(time):
     return secs
 
 # 处理游戏时长，将其转换成stage
+# def stage(time, secs):
+#     if (time <= int(secs / 3)):
+#         return 1
+#     elif (int(secs / 3) < time <= int(secs / 3 * 2)):
+#         return 2
+#     else:
+#         return 3
+
+# 处理游戏时长，将其转换成stage
 def stage(time, secs):
-    if (time <= int(secs / 3)):
+    if (time <= 420):
         return 1
-    elif (int(secs / 3) < time <= int(secs / 3 * 2)):
+    elif (420 < time <= 900):
         return 2
     else:
         return 3
 
 # 放入replay名字(56003:73829)
-for pa in range(68513, 73830):
-    path = 'D:/SC2/SC/' + str(pa) + '.SC2Replay'
+for pa in range(800, 801):
+    path = 'D:/SC/HeroMarine/' + str(pa) + '.SC2Replay'
     deter = False
     while(deter == False):
         if os.path.exists(path):
-            path = 'D:/SC2/SC/' + str(pa) + '.SC2Replay'
+            path = 'D:/SC/HeroMarine/' + str(pa) + '.SC2Replay'
             break
         else:
             pa += 1
-            path = 'D:/SC2/SC/' + str(pa) + '.SC2Replay'
+            path = 'D:/SC/HeroMarine/' + str(pa) + '.SC2Replay'
 
     # 导入replay
     sc2reader.configure(debug=True)
@@ -108,6 +120,8 @@ for pa in range(68513, 73830):
         replay = sc2reader.load_replay(path, load_level=4)
         replay.load_map()
     except:
+        print("Replay: ", pa)
+        print(replay.player)
         continue
 
     # 一些比赛的信息。
@@ -119,11 +133,22 @@ for pa in range(68513, 73830):
     # print(replay.real_type)
     # print(replay.real_length)
     # print(replay.player)
-    print(replay.player)
+    if str(replay.winner.players[0].name) == str(replay.player[1].name):
+        print("The WINNER is: Player 1")
+        print("The LOSER is: Player 2")
+    else:
+        print("The WINNER is: Player 2")
+        print("The LOSER is: Player 1")
     # 提取到地图的长宽数值
     height = replay.map.map_info.height
     width = replay.map.map_info.width
     secs = seconds(replay.real_length)
+    playerID = 0
+    # DPGZest, Maru, PtitDrogo
+    if (replay.player[1].name == "HeroMarine") or (replay.player[1].name == "Neeb") or (replay.player[1].name == "Elazer") or (replay.player[1].name == "HeRoMaRinE"):
+        playerID = 0
+    else:
+        playerID = 1
 
     out_string = []
     list1 = []
@@ -138,7 +163,8 @@ for pa in range(68513, 73830):
        if (event.second > 0):
            # 将选中的部队存入一个列表（根据选中的部队进行“进攻”单位的判断）
            if (event.name == "SelectionEvent") and (event.player != None):
-               if (event.player.sid == 0) or (event.player.sid == 1):
+               # if (event.player.sid == 0) or (event.player.sid == 1):
+               if (event.player.sid == playerID):
                    if len(event.new_units):
                        if event.player.sid == 0:
                            list1.clear()
@@ -151,11 +177,13 @@ for pa in range(68513, 73830):
 
            if event.name == "TargetUnitCommandEvent":
                if (event.ability_name == "UnloadTargetWarpPrism") or (event.ability_name == "UnloadTargetMedivac") or (event.ability_name == "UnloadTargetOverlord"):
-                   time = stage(event.second, secs)
-                   area = location(event.x, event.y, height, width)
-                   string = "Player " + str(event.player.sid + 1) + " " + event.target.name + " is Unloading at area " + str(area) + " in stage " + str(time)
-                   out_string.append(string)
-                   print(string)
+                    if event.player.sid == playerID:
+                       time = stage(event.second, secs)
+                       area = location(event.x, event.y, height, width)
+                       # string = "Player " + str(event.player.sid + 1) + " " + event.target.name + " is Unloading at area " + str(area) + " in stage " + str(time)
+                       string = event.target.name + " is Unloading at area " + str(area) + " in stage " + str(time)
+                       out_string.append(string)
+                       # print(string)
                # 输出进攻事件（右键攻击也包括在内）
                # if (event.ability_name == "Attack") or (event.ability_name == "RightClick"):
                #     if (event.target.race != "None") and (event.target.race != "Neutral"):
@@ -179,43 +207,59 @@ for pa in range(68513, 73830):
 
             # 输出移动事件
            if (event.name == "TargetPointCommandEvent") and (event.player != None):
-               if event.player.sid == 0:
+               if event.player.sid == playerID:
                    for temp in list1:
                        if temp.is_army == True:
                            time = stage(event.second, secs)
                            area = location(event.x, event.y, height, width)
-                           string = "Player " + str(event.player.sid+1) + " " + remove_id(temp) + " Moves to area " + str(area) + " in stage " + str(time)
+                           # string = "Player " + str(event.player.sid+1) + " " + remove_id(temp) + " Moves to area " + str(area) + " in stage " + str(time)
+                           string = remove_id(temp) + " Moves to area " + str(area) + " in stage " + str(time)
                            out_string.append(string)
                            # print(string)
 
-               elif event.player.sid == 1:
-                   for temp in list2:
-                       if temp.is_army == True:
-                           time = stage(event.second, secs)
-                           area = location(event.x, event.y, height, width)
-                           string = "Player " + str(event.player.sid+1) + " " + remove_id(temp) + " Moves to area " + str(area) + " in stage " + str(time)
-                           out_string.append(string)
-                           # print(string)
+               # 单选一方选手而comment掉
+               # elif event.player.sid == 1:
+               #     for temp in list2:
+               #         if temp.is_army == True:
+               #             time = stage(event.second, secs)
+               #             area = location(event.x, event.y, height, width)
+               #             string = "Player " + str(event.player.sid+1) + " " + remove_id(temp) + " Moves to area " + str(area) + " in stage " + str(time)
+               #             out_string.append(string)
+               #             print(string)
 
             # 输出创建建筑事件
            if (event.name == "UnitInitEvent"):
                if (event.unit.race != "None") and (event.unit.race != "Neutral") and (event.unit.is_building == True):
-                   secs = seconds(replay.real_length)
-                   area = location(event.x, event.y, height, width)
-                   time = stage(event.second, secs)
-                   string = "Player " + str(event.control_pid) + " Build a " + remove_id(event.unit_type_name) + " at area " + str(area) + " in stage " + str(time)
-                   out_string.append(string)
-                   # print(string)
+                   if event.control_pid == (playerID+1):
+                       secs = seconds(replay.real_length)
+                       area = location(event.x, event.y, height, width)
+                       time = stage(event.second, secs)
+                       # string = "Player " + str(event.control_pid) + " Build a " + remove_id(event.unit_type_name) + " at area " + str(area) + " in stage " + str(time)
+                       string = "Build a " + remove_id(event.unit_type_name) + " at area " + str(area) + " in stage " + str(time)
+                       out_string.append(string)
+                       # print(string)
+
+               if (event.unit.race != "None") and (event.unit.race != "Neutral") and (event.unit.is_army == True):
+                   if event.control_pid == (playerID + 1):
+                       secs = seconds(replay.real_length)
+                       area = location(event.x, event.y, height, width)
+                       time = stage(event.second, secs)
+                       # string = "Player " + str(event.control_pid) + " Generate a " + remove_id(event.unit_type_name) + " at area " + str(area) + " in stage " + str(time)
+                       string = "Generate a " + remove_id(event.unit_type_name) + " at area " + str(area) + " in stage " + str(time)
+                       out_string.append(string)
+                       # print(string)
 
            # 输出创建单位事件
            if (event.name == "UnitBornEvent"):
                if (event.unit.race != "None") and (event.unit.race != "Neutral") and (event.unit.is_army == True):
-                   secs = seconds(replay.real_length)
-                   area = location(event.x, event.y, height, width)
-                   time = stage(event.second, secs)
-                   string = "Player " + str(event.control_pid) + " Generate a " + remove_id(event.unit_type_name) + " at area " + str(area) + " in stage " + str(time)
-                   out_string.append(string)
-                   # print(string)
+                   if event.control_pid == (playerID + 1):
+                       secs = seconds(replay.real_length)
+                       area = location(event.x, event.y, height, width)
+                       time = stage(event.second, secs)
+                       # string = "Player " + str(event.control_pid) + " Generate a " + remove_id(event.unit_type_name) + " at area " + str(area) + " in stage " + str(time)
+                       string = "Generate a " + remove_id(event.unit_type_name) + " at area " + str(area) + " in stage " + str(time)
+                       out_string.append(string)
+                       # print(string)
 
            # 输出死亡事件
            if ((event.name == "UnitDiedEvent") and ((event.killing_player_id  == 1) or (event.killing_player_id  == 2))):
@@ -231,48 +275,50 @@ for pa in range(68513, 73830):
 
                # 过滤掉中立单位
                if (event.unit.race != "Neutral") and (event.unit.race != "None") and (event.unit.owner != "None"):
-                   if(event.killing_player_id != Search[event.unit_id]):
+                   if event.killing_player_id != Search[event.unit_id]:
                        if (event.unit.is_building == True):
                            if remove_id(event.killing_unit) != "None":
-                               if (event.killing_player_id == 1):
+                               if event.killing_player_id == (playerID+1):
                                    secs = seconds(replay.real_length)
                                    area = location(event.x, event.y, height, width)
                                    time = stage(event.second, secs)
-                                   string = "Player " + str(event.killing_player_id) + " " + remove_id(event.killing_unit) + " Destroyed" + " Player 2 " + remove_id(event.unit) + " at area " + str(area) + " in stage " + str(time)
+                                   # string = "Player " + str(event.killing_player_id) + " " + remove_id(event.killing_unit) + " Destroyed" + " Player 2 " + remove_id(event.unit) + " at area " + str(area) + " in stage " + str(time)
+                                   string = remove_id(event.killing_unit) + " Destroyed" + remove_id(event.unit) + " at area " + str(area) + " in stage " + str(time)
                                    out_string.append(string)
                                    # print(string)
 
-                               elif (event.killing_player_id == 2):
-                                   secs = seconds(replay.real_length)
-                                   area = location(event.x, event.y, height, width)
-                                   time = stage(event.second, secs)
-                                   string = "Player " + str(event.killing_player_id) + " " + remove_id(event.killing_unit) + " Destroyed" + " Player 1 " + remove_id(event.unit) + " at area " + str(area) + " in stage " + str(time)
-                                   out_string.append(string)
+                               # elif (event.killing_player_id == 2):
+                               #     secs = seconds(replay.real_length)
+                               #     area = location(event.x, event.y, height, width)
+                               #     time = stage(event.second, secs)
+                               #     string = "Player " + str(event.killing_player_id) + " " + remove_id(event.killing_unit) + " Destroyed" + " Player 1 " + remove_id(event.unit) + " at area " + str(area) + " in stage " + str(time)
+                               #     out_string.append(string)
                                    # print(string)
 
                        else:
                            if remove_id(event.killing_unit) != "None":
-                               if (event.killing_player_id == 1):
+                               if event.killing_player_id == (playerID+1):
                                    secs = seconds(replay.real_length)
                                    area = location(event.x, event.y, height, width)
                                    time = stage(event.second, secs)
-                                   string = "Player " + str(event.killing_player_id) + " " + remove_id(event.killing_unit) + " Killed" + " Player 2 " + remove_id(event.unit) + " at area " + str(area) + " in stage " + str(time)
+                                   # string = "Player " + str(event.killing_player_id) + " " + remove_id(event.killing_unit) + " Killed" + " Player 2 " + remove_id(event.unit) + " at area " + str(area) + " in stage " + str(time)
+                                   string = remove_id(event.killing_unit) + " Killed" + remove_id(event.unit) + " at area " + str(area) + " in stage " + str(time)
                                    out_string.append(string)
                                    # print(string)
 
-                               elif (event.killing_player_id == 2):
-                                   secs = seconds(replay.real_length)
-                                   area = location(event.x, event.y, height, width)
-                                   time = stage(event.second, secs)
-                                   string = "Player " + str(event.killing_player_id) + " " + remove_id(event.killing_unit) + " Killed" + " Player 1 " + remove_id(event.unit) + " at area " + str(area) + " in stage " + str(time)
-                                   out_string.append(string)
-                                   # print(string)
+                               # elif (event.killing_player_id == 2):
+                               #     secs = seconds(replay.real_length)
+                               #     area = location(event.x, event.y, height, width)
+                               #     time = stage(event.second, secs)
+                               #     string = "Player " + str(event.killing_player_id) + " " + remove_id(event.killing_unit) + " Killed" + " Player 1 " + remove_id(event.unit) + " at area " + str(area) + " in stage " + str(time)
+                               #     out_string.append(string)
+                               #     # print(string)
 
-    write = 'D:/SC2/SCIII/' + str(pa) + '.csv'
+    write = 'D:/SC/HeroMarinec/' + str(pa) + '.csv'
     f = open(write, 'w', encoding='utf-8')
     csv_write = csv.writer(f)
     for i in out_string:
         csv_write.writerow([i])
 
-    print(write)
+    # print(write)
 
--- Tokenised.py
+++ Tokenised.py
@@ -1,8 +1,11 @@
 from sklearn.cluster import KMeans
+from sklearn.cluster import DBSCAN
 import matplotlib.pylab as plt
 from sklearn.metrics import silhouette_score
 import copy
 import gc
+import math
+from umap import UMAP
 from sklearn.decomposition import PCA
 # import umap
 # from sklearn.preprocessing import StandardScaler
@@ -10,25 +13,30 @@ import numpy as np
 # np.set_printoptions(threshold=np.inf)
 import os
 import csv
+import sys
+
+def min_n(list1, num):
+    return sorted(list1, reverse=False)[:num]
 
 dataset = []
 count = 0
+replay = 0
 # 记录所有事件
 feature_set = []
 # 扫描一遍后，所有事件维度的dictionary
 data_dictionary = dict()
 
 # 读入csv文件
-for pa in range(56003, 73830):
-    path = 'D:/SC2/SCIII/' + str(pa) + '.csv'
+for pa in range(1, 726):
+    path = 'D:/SC/HeroMarineccc/' + str(pa) + '.csv'
     deter = False
     while(deter == False):
         if os.path.exists(path):
-            path = 'D:/SC2/SCIII/' + str(pa) + '.csv'
+            path = 'D:/SC/HeroMarineccc/' + str(pa) + '.csv'
             break
         else:
             pa += 1
-            path = 'D:/SC2/SCIII/' + str(pa) + '.csv'
+            path = 'D:/SC/HeroMarineccc/' + str(pa) + '.csv'
 
     # 导入csv
     try:
@@ -48,17 +56,18 @@ for pa in range(56003, 73830):
     feature_set.append(dataset)
     dataset = []
 
-# 输出一共多少event
-print(len(feature_set))
-compared_dictionary = dict()
+# 输出一共多少replays
+# 输出一共多少events
+print("There are %d events" % len(feature_set))
+# feature_set = np.asarray(feature_set, dtype=np.float16)
 feature = []
 for i in range(0, len(feature_set)):
-
+    compared_dictionary = dict()
     # 每次都初始化到value=0的dictionary
     compared_dictionary = copy.deepcopy(data_dictionary)
     for j in range(0, len(feature_set[i])):
         if str(feature_set[i][j]) in compared_dictionary.keys():
-
+            # tag k=3 player clustering 最相近的几个cluster提出来做evaluation
             # 匹配成功后，将dictionary里的值加一
             count = compared_dictionary[feature_set[i][j]]
             count += 1
@@ -66,95 +75,119 @@ for i in range(0, len(feature_set)):
 
     # 单个replay的全部event匹配完成后，将字典转成list存入feature 2d list
     feature.append(list(compared_dictionary.values()))
+    # print(sys.getsizeof(compared_dictionary))
     del compared_dictionary
-    gc.collect()
+    # print(sys.getsizeof(feature))
+    # gc.collect()
+# print(feature[0:2])
 
 # 测试dimension
+# print(feature)
 # print(len(feature))
 # print(len(feature[0]))
 # print(len(feature[10]))
 # print(len(feature[100]))
 
-lower_dimension = PCA(n_components=2).fit_transform(feature)
-# print(lower_dimension)
+# PCA + KMeans
+lower_dimension = PCA(n_components=50).fit_transform(feature)
+embedding = UMAP(random_state=42).fit_transform(feature)
+print(len(embedding))
 scores = []
-for i in range(2, 11):
+for i in range(3, 11):
     kmeans = KMeans(init='k-means++', n_clusters=i, max_iter=300, n_init=10, random_state=0)
-    kmeans.fit(lower_dimension)
-    score = silhouette_score(lower_dimension, kmeans.labels_, metric='euclidean', sample_size=len(lower_dimension))
+    kmeans.fit(embedding)
+    score = silhouette_score(embedding, kmeans.labels_, metric='euclidean', sample_size=len(embedding))
     scores.append(score)
-print(scores)
-max_num = scores.index(max(scores))
-max_num = max_num + 1
-print(max_num)
 
-km = KMeans(init='k-means++', n_clusters=max_num, max_iter=300, n_init=10, random_state=0)
-y_label = km.fit_predict(lower_dimension)
-
-plt.scatter(lower_dimension[:, 0], lower_dimension[:, 1], marker='o', edgecolors='k', s=30)
-centroid = km.cluster_centers_
-plt.scatter(lower_dimension[y_label == 0, 0], lower_dimension[y_label == 0, 1], s=100, c='pink', label='1')
-plt.scatter(lower_dimension[y_label == 1, 0], lower_dimension[y_label == 1, 1], s=100, c='black', label='2')
-plt.scatter(lower_dimension[y_label == 2, 0], lower_dimension[y_label == 2, 1], s=100, c='orange', label='3')
-plt.scatter(lower_dimension[y_label == 3, 0], lower_dimension[y_label == 3, 1], s=100, c='red', label='4')
-plt.scatter(lower_dimension[y_label == 4, 0], lower_dimension[y_label == 4, 1], s=100, c='green', label='5')
-plt.scatter(lower_dimension[y_label == 5, 0], lower_dimension[y_label == 5, 1], s=100, c='grey', label='6')
-plt.scatter(lower_dimension[y_label == 6, 0], lower_dimension[y_label == 6, 1], s=100, c='yellow', label='7')
-plt.scatter(lower_dimension[y_label == 7, 0], lower_dimension[y_label == 7, 1], s=100, c='blue', label='8')
-plt.scatter(lower_dimension[y_label == 8, 0], lower_dimension[y_label == 8, 1], s=100, c='purple', label='9')
-plt.scatter(lower_dimension[y_label == 9, 0], lower_dimension[y_label == 9, 1], s=100, c='brown', label='10')
-
+best_num = scores.index(np.max(scores))
+best_num = best_num + 2
+#
+km = KMeans(init='k-means++', n_clusters=best_num, max_iter=300, n_init=10, random_state=0)
+y_label = km.fit_predict(embedding)
+# # centroid = km.cluster_centers_
+for i in range(0, best_num):
+    plt.scatter(embedding[y_label == i, 0], embedding[y_label == i, 1], s=30, edgecolors='k', label='cluster'+str(i+1))
+plt.legend(bbox_to_anchor=(1.35, 1), loc='upper right')
+plt.title("Clustering replays of three different race players\nby K-Means")
+plt.tight_layout()
 plt.show()
 
+# t-sne
+# 求聚类的方差和平均数
+db = DBSCAN(eps=0.625, min_samples=30)
+y_pred = db.fit_predict(embedding)
+plt.scatter(embedding[y_pred == -1, 0], embedding[y_pred == -1, 1], s=30, c='black', edgecolors='k', label='outlier(s)')
+for i in range(0, max(db.labels_)+1):
+    plt.scatter(embedding[y_pred == i, 0], embedding[y_pred == i, 1], s=30, edgecolors='k', label='cluster'+str(i+1))
+plt.legend(bbox_to_anchor=(1.35, 1), loc='upper right')
+plt.title("Clustering replays of three different race players\nby DBSCAN")
+plt.tight_layout()
+plt.show()
 
-# 暂时用不到
-# compared_dictionary[str(dataset[i])] = 0
-# count = compared_dictionary[dataset[i]]
-# count += 1
-# compared_dictionary[str(dataset[i])] = count
+print("\n")
+print("K-Means:")
+number = 0
+KMx = []
+KMy = []
+for i in range(0, best_num):
+    number = i + 1
+    KMx.append(np.mean(embedding[y_label == i, 0]))
+    KMy.append(np.mean(embedding[y_label == i, 1]))
+    print("Cluster %d mean x-axis:" % number, np.mean(embedding[y_label == i, 0]))
+    print("Cluster %d mean y-axis:" % number, np.mean(embedding[y_label == i, 1]))
+    # print("Cluster %d variance x-axis:" % number, np.var(embedding[y_label == i, 0]))
+    # print("Cluster %d variance y-axis:" % number, np.var(embedding[y_label == i, 1]))
 
-# feature.append(list(compared_dictionary.values()))
+print("\n")
+print("DBSCAN:")
+number = 0
+DBx = []
+DBy = []
+for i in range(0, max(db.labels_)+1):
+    number = i + 1
+    DBx.append(np.mean(embedding[y_pred == i, 0]))
+    DBy.append(np.mean(embedding[y_pred == i, 1]))
+    print("Cluster %d mean x-axis:" % number, DBx[i])
+    print("Cluster %d mean y-axis:" % number, DBy[i])
+    # print("Cluster %d variance x-axis:" % number, np.var(embedding[y_pred == i, 0]))
+    # print("Cluster %d variance y-axis:" % number, np.var(embedding[y_pred == i, 1]))
 
+print("\n")
+print(KMx)
+print(KMy)
+dislist = []
+disdict = dict()
+for count in range(0, best_num):
+    result = []
+    for i in range(0, len(embedding)):
+        tempx = abs(embedding[i, 0] - KMx[count])
+        tempy = abs(embedding[i, 1] - KMy[count])
+        distance = math.sqrt(pow(tempy, 2) + pow(tempx, 2))
+        disdict[str(distance)] = str(i)
+        dislist.append(distance)
+    dislist = min_n(dislist, 10)
+    for j in range(0, len(dislist)):
+        result.append(str(disdict[str(dislist[j])]))
+    print(result)
+    dislist.clear()
+    disdict.clear()
 
-# length = len(dataset)
-# for i in range(length):
-#     dataset[i] += '\n'
-# vector = np.array([[0]*7]*length).
-# tokens = 0
-# count = 0
-# col = 0
-# row = 0
-# for i in dataset:
-#     for j in range(len(i)):
-#         if dataset[row][j] == ' ':
-#             vector[row][col] = count
-#             count = 0
-#             col += 1
-#         elif dataset[row][j] == '\n':
-#             vector[row][col] += count
-#             count = 0
-#             col = 0
-#             row += 1
-#         else:
-#             if dataset[row][j].isdigit() == True:
-#                 count += int(dataset[row][j])
-#             else:
-#                 count += ord(dataset[row][j])
-# # print(vector)
+print("\n")
+print(DBx)
+print(DBy)
+for count in range(0, max(db.labels_)+1):
+    result = []
+    for i in range(0, len(embedding)):
+        tempx = abs(embedding[i, 0] - DBx[count])
+        tempy = abs(embedding[i, 1] - DBy[count])
+        distance = math.sqrt(pow(tempy, 2) + pow(tempx, 2))
+        disdict[str(distance)] = str(i)
+        dislist.append(distance)
+    dislist = min_n(dislist, 10)
+    for j in range(0, len(dislist)):
+        result.append(str(disdict[str(dislist[j])]))
+    print(result)
+    dislist.clear()
+    disdict.clear()
 #
-# # if dataset[1] == dataset[0]:
-# #     print(1)
-#
-# list1 = []
-# count = 0
-# for i in range(length):
-#     for j in range(length):
-#         if i != j:
-#             if dataset[i] == dataset[j]:
-#                 # if dataset[j][0] == '2':
-#                 count += 1
-#     list1.append(count)
-#     count = 0
-# print("The number of first ten events: ")
-# print(list1[:10])
-# # print(len(list1))
+#     # plt.annotate('cluster' + str(i + 1), (centroid[i, 0], centroid[i, 1]))
